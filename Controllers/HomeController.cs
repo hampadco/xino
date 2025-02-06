478ef62cda90202725f2976db08c63e6c7b63ac5
Abolfazl ASeM <a.semnani12@gmail.com>
2025-02-06 14:22:05 +0330

    our works

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,5 +18,16 @@ public class HomeController : Controller
         ViewBag.Title = "زینو سازه | درباره ما";
         return View();
     }
+    public IActionResult Services()
+    {
+        ViewBag.MetaDescription = "خدمات مهندسی و ساختمانی زینو سازه شامل طراحی و ساخت انواع سازه های فلزی و پیش ساخته همراه پشتیبانی و خدمات پس از فروش میباشد.";
+        ViewBag.Title = "زینو سازه | خدمات";
+        return View();
+    }
+    public IActionResult Portfolio(){
+        ViewBag.MetaDescription = "نمونه کارهای زینو سازه شامل انواع سازه های فلزی و پیش ساخته در نمایشگاه های مختلف می باشد.";
+        ViewBag.Title = "زینو سازه | نمونه کارها";
+        return View();
+    }
 
 }
--- obj/Debug/net9.0/xino.AssemblyInfo.cs
+++ obj/Debug/net9.0/xino.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2622e5b910e55e09af06b0896ba234cb3218fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cadba7e4e34e4d8b5b31daf244153f0e45521e8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("xino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
